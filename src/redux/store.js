a5d304b133a169a2599c795336fa067802d7ddea
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2018-01-31 14:26:04 +0100

    Updated store.js with the most commonly used operators

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,12 @@ import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/timer';
 
 import history from '../utils/history';
 
